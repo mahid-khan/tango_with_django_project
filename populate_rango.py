65f0a4864b3139a2c9602de7a02150789febd2f4
Mahid Khan <2725051k@student.gla.ac.uk>
2024-02-08 22:58:27 +0000

    Completed 6.2 Showing Categories on Rango’s Homepage

--- populate_rango.py
+++ populate_rango.py
@@ -29,9 +29,9 @@ def populate():
          'url':'http://flask.pocoo.org'}
     ]
 
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages}}
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}}
 
     for cat, cat_data in cats.items():
         c = add_cat(cat)
@@ -51,8 +51,10 @@ def add_page(cat, title, url, views=0):
     return p
             
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
